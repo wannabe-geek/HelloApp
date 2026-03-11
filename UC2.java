61c2e235d30b92a69a2cb42e8b016cecf497c178
wannabe-geek <vsryashwanth.007@gmail.com>
2026-03-11 09:39:43 +0530

    Coded UC2: Display Hello with Command-Line Argument

---
+++ UC2.java
@@ -0,0 +1,13 @@
+public class UC2{
+
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
+    }
+}
